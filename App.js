5b646fb215a8f4a12de9427b56e1f928efb83683
Jonas_Nunn <jonasnunn@gmail.com>
2022-06-09 14:39:29 -0600

    Added Login.js and made it the defualt screen

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Counter from './Counter.js';
+import Login from './Login.js';
 import SettingsScreen from './SettingsScreen.js';
 import Home from './Home.js';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {useState} from "react";
 
 // import Icons from "./Icons";
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  if(userLoggedIn){
+
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -53,7 +58,14 @@ export default function App() {
       </Tab.Navigator>
     </NavigationContainer>
   );
-
+  
+  } else{
+    return(
+      <View>
+        <Login/>
+      </View>
+    )
+  }
 
 }
 
