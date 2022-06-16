caa954dadc048619ceb05ac14b40428ad0181b87
Jonas_Nunn <jonasnunn@gmail.com>
2022-06-16 14:13:41 -0600

    added login button and two factor auth.

--- Login.js
+++ Login.js
@@ -1,5 +1,18 @@
 import {useState} from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity, Text} from "react-native";
+
+
+const sendText = async (phoneNumber) => {
+
+  // using fetch do a POST to https://dev.stedi.me/twofactorlogin/469-744-4698
+  await fetch('https://dev.stedi.me/twofactorlogin/'+phoneNumber,{
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/text'
+  }
+  });
+  console.log("PhoneNumber: ",phoneNumber);
+}
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -13,6 +26,14 @@ const Login = () => {
         value={phoneNumber}
         placeholder="801-555-1212"
       />
+      <TouchableOpacity
+      style={styles.button}
+      onPress={()=>{sendText(phoneNumber)}}
+      >
+        <Text>Send Text</Text>
+      </TouchableOpacity>
+
+      
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
@@ -20,7 +41,17 @@ const Login = () => {
         placeholder="1234"
         keyboardType="numeric"
         secureTextEntry={true}
-      />
+      /> 
+      
+      <TouchableOpacity
+      style={styles.button}
+      onPress={()=>{sendText(phoneNumber)}}
+      >
+        <Text>Login</Text>
+      </TouchableOpacity>
+
+
+
     </SafeAreaView>
   );
 };
@@ -34,6 +65,11 @@ const styles = StyleSheet.create({
   },
   mainView:{
       marginTop:100
+    },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
     }
 });
 
